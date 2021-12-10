8ab69ff7a1e11858bd037a4cc12774f82194dc85
profoundsea25 <profoundsea25@gmail.com>
2021-12-10 10:57:09 +0900

    edit-icon 유저 식별

--- js/profile.js
+++ js/profile.js
@@ -31,6 +31,7 @@ function showMyPageSettings() {
         $('#profile-intro-text').append(profileTextModifier)
     } else {
         let profilePictureButtonInactive = `<img id="user-profile-image" src="" alt="profile image">`
+        $('#edit-icon').hide()
         $('#profile-pic').append(profilePictureButtonInactive)
     }
 }
@@ -172,7 +173,7 @@ function saveUserProfileIntroText(userId) {
         return alert("상태 메세지는 100자를 넘길 수 없습니다.")
     }
 
-    $.ajax({ /// 상태 메시지 하나 보내는 것입니다 회원님들...
+    $.ajax({
         type : "POST",
         url : `${WEB_SERVER_DOMAIN}/profile/intro/${userId}`,
         contentType: "application/json",
